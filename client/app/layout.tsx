e7622dd28858705a7314aa0ba2f0b0febb8487b6
ThommysArt <keabou.thomson@ictuniversity.edu.cm>
2024-07-08 10:25:40 +0100

    init project

---
+++ client/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google"
+import "./globals.css";
+
+import { cn } from "@/lib/utils"
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
+ 
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
